0a224eecacc774f689007eaddd4deaacbd565180
kalpeshbhonge58 <kalpeshbhonge58@gmail.com>
2026-08-20 18:07:42 +0530

    feat: update app structure with event handling examples and style adjustments

--- angular-21-from-scratch/src/app/app.ts
+++ angular-21-from-scratch/src/app/app.ts
@@ -1,39 +1,55 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Logout } from './logout/logout';
-import { Login } from './login/login';
-import { Profile } from './profile/profile';
+// import { Logout } from './logout/logout';
+// import { Login } from './login/login';
+// import { Profile } from './profile/profile';
 
 @Component({
-  imports: [RouterOutlet,Login,Profile,Logout],
+  imports: [RouterOutlet],
+ // imports: [RouterOutlet,Login,Profile,Logout],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   //Property Example
-  name = 'Kalpesh'
+  //name = 'Kalpesh'
   //Function Binding Example
-  count = 0;
-  Callme() {
-    alert("Hello, This is first Angular app");
-  }
+  // count = 0;
+  // Callme() {
+  //   alert("Hello, This is first Angular app");
+  // }
 
   //Counter Function Binding Example
 
-  Counter(action: string) {
-    if (action === 'increment') {
-      this.count++;
-    } else if (action === 'decrement') {
-      this.count > 0 && this.count--;
-    }
-    //Function inside another Function Binding Example call here
-    this.JointComponent();
-  }
+  // Counter(action: string) {
+  //   if (action === 'increment') {
+  //     this.count++;
+  //   } else if (action === 'decrement') {
+  //     this.count > 0 && this.count--;
+  //   }
+  //   //Function inside another Function Binding Example call here
+  //   this.JointComponent();
+  // }
 
   //Function inside another Function Binding Example
-  JointComponent(){
-    alert("This is joint component of Login, Profile and Logout");
-  }
+  // JointComponent(){
+  //   alert("This is joint component of Login, Profile and Logout");
+  // }
 
+  //Input Type Event Example
+  // name = '';
+  // onInput(event: Event) {
+  //   const inputElement = event.target as HTMLInputElement;
+  //   this.name = inputElement.value;
+  // }
+
+  // handleInput(event: Event) {
+  //   console.log('Input event:', event);
+    handleInput(event: string) {
+    console.log(event);
+    
+    // const inputElement = event.target as HTMLInputElement;
+    // this.name = inputElement.value;
   }
+}
